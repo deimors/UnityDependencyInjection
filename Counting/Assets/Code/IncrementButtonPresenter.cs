ac5876020c4be074a0427e578b674075c3a4be72
deimors <deimors@gmail.com>
2019-01-18 17:44:18 -0600

    Added IncrementButtonPresenter

---
+++ Counting/Assets/Code/IncrementButtonPresenter.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Code
+{
+	public class IncrementButtonPresenter : MonoBehaviour
+	{
+		public Button IncrementButton;
+
+		public void Initialize(Counter counter)
+			=> IncrementButton.onClick.AddListener(counter.Increment);
+	}
+}
