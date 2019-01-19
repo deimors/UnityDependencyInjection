9fe06edf3dc7b944d27fccbacc7c7e471a0e6d53
deimors <deimors@gmail.com>
2019-01-18 18:01:37 -0600

    Add CurrentCountTextPresenter to Current Count Text

---
+++ Counting/Assets/Code/CurrentCountTextPresenter.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Code
+{
+	public class CurrentCountTextPresenter : MonoBehaviour
+	{
+		public Text CurrentCountText;
+
+		public void Initialize(Counter counter)
+			=> counter.Incremented += newCount => CurrentCountText.text = newCount.ToString();
+	}
+}
