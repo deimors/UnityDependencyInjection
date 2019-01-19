4ed77d8f201a26259028c15b3c63b6a37d99177b
deimors <deimors@gmail.com>
2019-01-18 18:38:13 -0600

    Add SceneInstaller and SceneContext

---
+++ Counting/Assets/Code/SceneInstaller.cs
@@ -0,0 +1,12 @@
+﻿using Zenject;
+
+namespace Assets.Code
+{
+	public class SceneInstaller : MonoInstaller
+	{
+		public override void InstallBindings()
+		{
+			Container.Bind<Counter>().AsSingle();
+		}
+	}
+}
